3611e0851ae391c388d4d017597932144e3371ba
FaresAhmed23 <153539187+FaresAhmed23@users.noreply.github.com>
2025-02-10 07:33:10 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,62 +5,52 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
-	env: {
-    		PRISMA_SCHEMA_PATH: "prisma/schema.prisma"
-  	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-	eslint: {
-		// Disable ESLint during builds
-		ignoreDuringBuilds: true,
-	},
-	images: {
-		domains: ["uploadthing.com", "utfs.io"],
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "github.com",
-			},
-			{
-				protocol: "https",
-				hostname: "lh3.googleusercontent.com",
-			},
-			{
-				protocol: "https",
-				hostname: "utfs.io",
-			},
-			{
-				protocol: "https",
-				hostname: "avatars.githubusercontent.com",
-			},
-		],
-	},
-	//@ts-ignore
-	webpack: (config, { isServer }) => {
-		if (!isServer) {
-			if (!config.resolve) {
-				config.resolve = {};
-			}
-			if (!config.resolve.fallback) {
-				config.resolve.fallback = {};
-			}
-			config.resolve.fallback = {
-				...config.resolve.fallback,
-				fs: false,
-				net: false,
-				tls: false,
-				child_process: false,
-				debug: require.resolve("debug"),
-				util: require.resolve("util/"),
-				stream: require.resolve("stream-browserify"),
-				path: require.resolve("path-browserify"),
-				crypto: require.resolve("crypto-browserify"),
-			};
-		}
-		return config;
-	},
+  env: {
+    PRISMA_SCHEMA_PATH: "prisma/schema.prisma",
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["uploadthing.com", "utfs.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false,
+        debug: require.resolve("debug"),
+        util: require.resolve("util/"),
+        stream: require.resolve("stream-browserify"),
+        path: require.resolve("path-browserify"),
+        crypto: require.resolve("crypto-browserify"),
+      };
+    }
+    return config;
+  },
 };
 
-// Wrap the nextConfig with withNextIntl
 module.exports = withNextIntl(nextConfig);
