b00f95eb597d73023f3cf92fd2218a626e306226
FaresAhmed23 <153539187+FaresAhmed23@users.noreply.github.com>
2025-02-10 07:34:05 +0200

    Update navigation.ts

--- navigation.ts
+++ navigation.ts
@@ -1,17 +1,11 @@
+// navigation.ts
 "use client";
 
 import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
+export const locales = ["en", "ar"] as const;
 export const { Link, useRouter, usePathname, redirect } =
-	createSharedPathnamesNavigation({
-		locales: ["en", "ar"],
-		localePrefix: "always",
-		//@ts-ignore
-		pathnames: {
-			"/": "/",
-			"/sign-in": "/sign-in",
-			"/sign-up": "/sign-up",
-			"/onboarding": "/onboarding",
-			"/dashboard": "/dashboard",
-		},
-	});
+  createSharedPathnamesNavigation({
+    locales,
+    localePrefix: "always",
+  });
