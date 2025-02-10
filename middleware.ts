0130e4446711a60b2da3e52a93bdadb60ecff845
FaresAhmed23 <153539187+FaresAhmed23@users.noreply.github.com>
2025-02-10 07:34:46 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,61 +1,45 @@
+// middleware.ts
 import createMiddleware from "next-intl/middleware";
 import { withAuth } from "next-auth/middleware";
 import { NextRequest } from "next/server";
 
-const locales = ["ar", "en"];
+const locales = ["en", "ar"];
 const publicPages = ["/", "/sign-in", "/sign-up"];
 
 const intlMiddleware = createMiddleware({
-	locales,
-	defaultLocale: "en",
-	// Add these optional configurations
-	localePrefix: "always",
-	// Optional: Add default timezone and formats
-	//@ts-ignore
-	timeZone: "UTC",
-	formats: {
-		dateTime: {
-			short: {
-				day: "numeric",
-				month: "short",
-				year: "numeric",
-			},
-		},
-	},
+  locales,
+  defaultLocale: "en",
+  localePrefix: "always",
 });
 
 const authMiddleware = withAuth(
-	function onSuccess(req) {
-		return intlMiddleware(req);
-	},
-	{
-		callbacks: {
-			authorized: ({ token }) => token !== null,
-		},
-		pages: {
-			signIn: "/sign-in",
-		},
-	},
+  function onSuccess(req) {
+    return intlMiddleware(req);
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token !== null,
+    },
+    pages: {
+      signIn: "/sign-in",
+    },
+  }
 );
 
 export default function middleware(req: NextRequest) {
-	const publicPathnameRegex = RegExp(
-		`^(/(${locales.join("|")}))?(${publicPages.join("|")})?/?$`,
-		"i",
-	);
-	const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
+  const publicPathnameRegex = RegExp(
+    `^(/(${locales.join("|")}))?(${publicPages.join("|")})?/?$`,
+    "i"
+  );
+  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
 
-	if (isPublicPage) {
-		return intlMiddleware(req);
-	} else {
-		return (authMiddleware as any)(req);
-	}
+  if (isPublicPage) {
+    return intlMiddleware(req);
+  } else {
+    return authMiddleware(req);
+  }
 }
 
 export const config = {
-	matcher: [
-		"/((?!api|_next|.*\\..*).*)",
-		"/((?!api|_next|.*\\.|favicon.ico).*)",
-		"/",
-	],
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
 };
